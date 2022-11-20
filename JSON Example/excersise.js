8e27e4992dd9647bfb84971cc18b62dbee258845
Dessiey <abushay123@gmail.com>
2022-11-20 21:27:13 +0300

    more on java and script functions

--- JSON Example/excersise.js
+++ JSON Example/excersise.js
@@ -26,4 +26,25 @@ function displayMessage() {
   
   console.log(rectangleArea(10,20)) //The area of rectangle is 200
   console.log(rectangleArea(30,30)) //The area of rectangle is 900
+
+
+  function compoundInt(principle, rate, time, n) {
+    var interest = principle * (Math.pow((1 + (rate / n)), (n * time)));
+    return principle + interest;
+  }
   
+  console.log(compoundInt(20000,5,2,2)) //3021250
+  console.log(compoundInt(150000,25,1,2)) //27487500
+
+  function getFactorial(num) {
+    if(num==1){
+      return 1; // Termination condition
+    }
+    if(num==0 || num<0){
+      return 0; // Termination condition
+    }
+    return num*getFactorial(num-1);
+  }
+  
+  console.log(getFactorial(5)) // 120
+  console.log(getFactorial(12)) // 479001600
---
+++ Java/mainexamples.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+
+    int n = 5;
+    // for loop  
+    for (int i = 1; i <= n; ++i) {
+      System.out.println("Java is fun");
+    }
+  }
+
+  int sum = 0;
+    int n = 1000;
+
+    // for loop
+    for (int i = 1; i <= n; ++i) {
+      // body inside for loop
+      sum += i;     // sum = sum + i
+    }
+       
+    System.out.println("Sum = " + sum);
+  }
+}
